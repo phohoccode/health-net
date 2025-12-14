cf92cf771e5b4bd4d33943c0951b8371e2a1a510
phohoccode <qviet092@gmail.com>
2025-12-14 21:36:21 +0700

    feat: add shadcn button component and configure tailwind

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,12 @@
+import { Button } from "./components/ui/button";
+
 const App = () => {
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-100 min-w-screen">
-      Hello world
+      <div className="flex flex-col gap-4">
+        <p>Hello world</p>
+        <Button className="text-black">Click me</Button>
+      </div>
     </div>
   );
 };
