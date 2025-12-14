8d4b5dcef108909275b017b8af799c08fbdb37de
phohoccode <qviet092@gmail.com>
2025-12-14 19:41:40 +0700

    init project

---
+++ client/src/App.tsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100 min-w-screen">
+      Hello world
+    </div>
+  );
+};
+
+export default App;
